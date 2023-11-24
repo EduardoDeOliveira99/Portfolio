49dd12eed47e8ab5a24bf571ba79df90088d8cfb
EduardoDeOliveira99 <xceert@gmail.com>
2023-11-24 15:15:36 -0300

    header tags and json rename

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,4 +2,13 @@
 (async () => {
     const profileData = await getProfileData();
     
+
+    function convertProfileData (getProfileData){
+
+        const name = profileData.name;
+        const job = profileData.job;
+        const location = profileData.location;
+        const phone = profileData.phone;
+    }
+
 })()
